21962c3efa68f4f4ae51b41d492a4d3589f0c7be
krishnaenterprises0423 <krishnaenterprises0423@gmail.com>
2023-06-09 19:13:02 +0530

    ok

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -2,7 +2,7 @@ import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import UserProfile from "views/UserProfile/UserProfile.jsx";
 import Sales from "views/Sales/Sales.jsx";
 import Items from "views/Items/Items.jsx";
-import Category from "views/Football/Category.jsx";
+import GlobalCategory from "views/admin/GlobalCategory.jsx";
 import Jackpot from "views/Jackpot/Jackpot.jsx";
 import MobileMoney from "views/MobileMoney/MobileMoney.jsx";
 import CreditTransfers from "views/CreditTransfers/CreditTransfers.jsx";
@@ -15,7 +15,7 @@ const appRoutes = [
     { path: "/dashboard", sidebarName: "Dashboard", navbarName: "Dashboard", icon: Dashboard, component: DashboardPage },
     { path: "/items", sidebarName: "Items", navbarName: "Items", icon: LocalDrink, component: Items },
     { path: "/sales", sidebarName: "Sales", navbarName: "Sales", icon: LocalDrink, component: Sales },
-    { path: "/category", sidebarName: "Category", navbarName: "Category", icon: LibraryBooks, component: Category },
+    { path: "/category", sidebarName: "Category", navbarName: "Category", icon: LibraryBooks, component: GlobalCategory },
     { path: "/unit", sidebarName: "Unit", navbarName: "Unit", icon: BubbleChart, component: Jackpot },
     { path: "/countfreq", sidebarName: "Count Freq", navbarName: "Count Freq", icon: PhoneIphone, component: MobileMoney },
     { path: "/credit_transfers", sidebarName: "Credit Transfer", navbarName: "Credit Transfer", icon: Notifications, component: CreditTransfers },
---
+++ src/views/admin/GlobalCategory.jsx
@@ -0,0 +1,183 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Grid, Button, IconButton, DeleteIcon } from 'material-ui';
+import { AddAlert } from 'material-ui-icons';
+
+import AddItemModal from './Modals/AddGlobalCategory';
+import EditItemModal from './Modals/EditGlobalCategory';
+import UpdateItemModal from './Modals/UpdateGlobalCategory';
+import { RegularCard, ItemsTable, ItemGrid, Snackbar } from 'components';
+
+import Loader from '../../Loader';
+
+import { getAllItems, addItem } from '../../actions';
+
+class GlobalCategory extends Component {
+    state = {
+        notificationGroup: 'add',
+        showAddItemModal: false,
+        showEditItemModal: false,
+        showUpdateItemModal: false,
+        tr: false,
+        tc: false,
+    };
+
+    componentDidMount() {
+        this.props.getAllItems();
+    }
+
+    // Check if the user is super admin.
+    isSuperAdmin = () => {
+        return true; //this.props.user.role.name === 'super_admin';
+    };
+
+    tableHead = () => {
+        return this.isSuperAdmin()
+            ? ['Logo','Iden No.','Item Name','Retail Price', 'Purchase Price', 'Stock Qnt', 
+            'Create Dt','Updated Dt', 'Actions']
+            : ['Logo','Iden No.','Item Name','Retail Price', 'Purchase Price', 'Stock Qnt', 
+            'Created Dt','Updated Dt', 'Actions']
+    };
+
+    showNotification(place) {
+        var x = [];
+        x[place] = true;
+        this.setState(x);
+
+        setTimeout(function() {
+            x[place] = false;
+            this.setState(x);
+        }.bind(this), 3000);
+    }
+
+    notificationMessage = type => {
+        if (type === 'success') {
+            if (this.state.notificationGroup === 'add') {
+                return 'Item added successfully';
+            } else if (this.state.notificationGroup === 'edit') {
+                return 'Item edited successfully';
+            } else {
+                return 'Item updated successfully';
+            }
+        } else if (type === 'error') {
+            if (this.state.notificationGroup === 'edit') {
+                return 'Error Item could not be edited';
+            } else if (this.state.notificationGroup === 'add') {
+                return 'Error Item could not be added';
+            } else {
+                return 'Error Item could not be updated';
+            }
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Grid container>
+                    <ItemGrid xs={12} sm={12} md={12}>
+                        <RegularCard
+                            padIt
+                            cardTitle="Items"
+                            cardSubtitle="This is a list of all items in the system"
+                            button={
+                                this.isSuperAdmin() && (
+                                    <Button
+                                        style={ styles.addItemButton }
+                                        onClick={() => this.setState({ showAddItemModal: true, notificationGroup: 'add' })}>ADD ITEM</Button>
+                                )
+                            }
+                            content={
+                                <ItemsTable
+                                    tableHeaderColor="primary"
+                                    tableHead={this.tableHead()}
+                                    tableData={this.props.items}
+                                    editItem={() => this.setState({ showEditItemModal: true, notificationGroup: 'edit' })}
+                                    updateItem={() => this.setState({ showUpdateItemModal: true, notificationGroup: 'update' })}
+                                />
+                            }
+                        />
+                    </ItemGrid>
+                </Grid>
+
+                <Grid container justify='center'>
+                    <ItemGrid xs={12} sm={12} md={10} lg={8}>
+                        <Grid container>
+                            <ItemGrid xs={12} sm={12} md={4}>
+                                <Snackbar
+                                    place="tr"
+                                    color="success"
+                                    icon={AddAlert}
+                                    message={this.notificationMessage('success')}
+                                    open={this.state.tr}
+                                    closeNotification={() => this.setState({'tr': false})}
+                                    close
+                                />
+                            </ItemGrid>
+                        </Grid>
+                    </ItemGrid>
+                </Grid>
+
+                <Grid container justify='center'>
+                    <ItemGrid xs={12} sm={12} md={10} lg={8}>
+                        <Grid container>
+                            <ItemGrid xs={12} sm={12} md={4}>
+                                <Snackbar
+                                    place="tc"
+                                    color="danger"
+                                    icon={AddAlert}
+                                    message={this.notificationMessage('error')}
+                                    open={this.state.tc}
+                                    closeNotification={() => this.setState({'tc': false})}
+                                    close
+                                />
+                            </ItemGrid>
+                        </Grid>
+                    </ItemGrid>
+                </Grid>
+
+                <AddItemModal
+                    open={this.state.showAddItemModal}
+                    close={() => this.setState({ showAddItemModal: false })}
+                    addItem={this.props.addItem}
+                    refresh={this.props.getAllItems}
+                    successNotification={() => this.showNotification('tr')}
+                    errorNotification={() => this.showNotification('tc')}
+                />
+
+                <EditItemModal
+                    open={this.state.showEditItemModal}
+                    close={() => this.setState({ showEditItemModal: false })}
+                    refresh={this.props.getAllItems}
+                    successNotification={() => this.showNotification('tr')}
+                    errorNotification={() => this.showNotification('tc')}
+                />
+
+                <UpdateItemModal
+                    open={this.state.showUpdateItemModal}
+                    close={() => this.setState({ showUpdateItemModal: false })}
+                    refresh={this.props.getAllItems}
+                    successNotification={() => this.showNotification('tr')}
+                    errorNotification={() => this.showNotification('tc')}
+                />
+
+                <Loader open={this.props.show_item_loader} />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    const { user } = state.users;
+    const { items, show_item_loader } = state.items;
+
+    return { user, items, show_item_loader };
+};
+
+const styles = {
+    addItemButton: {
+        color: 'white',
+        backgroundColor: 'purple'
+    },
+};
+
+export default connect(mapStateToProps, { getAllItems, addItem })(GlobalCategory);
