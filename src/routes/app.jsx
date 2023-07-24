a898ad04e31c78fa872d119916902d9365d558aa
krishnaenterprises0423 <krishnaenterprises0423@gmail.com>
2023-07-24 21:01:34 +0530

    code update

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -9,9 +9,11 @@ import {
 
 const appRoutes = 
  [
-    { path: "/category", sidebarName: "Category", navbarName: "categoryList", icon: LibraryBooks, component: GlobalCategory },
-    { path: "/unit", sidebarName: "Units", navbarName: "Units", icon: BubbleChart, component: GlobalUnit },
-    { path: "/countfreq", sidebarName: "Count Freq", navbarName: "Count Freq", icon: PhoneIphone, component: GlobalCountFreq },
+    { path: "/globalcategorygroup", sidebarName: "Global Category Group", navbarName: "globalCategoryList", icon: LibraryBooks, component: GlobalCategory },
+    { path: "/globalcategory", sidebarName: "Global Category", navbarName: "globalCategoryGroupList", icon: PhoneIphone, component: GlobalCategory },
+    { path: "/globalunitgroup", sidebarName: "Global Unit Groups", navbarName: "GlobalUnitGroups", icon: BubbleChart, component: GlobalUnit },
+    { path: "/globalunit", sidebarName: "Global Units", navbarName: "GlobalUnits", icon: BubbleChart, component: GlobalUnit },
+    { path: "/globalcountfreq", sidebarName: "Global Freq", navbarName: "Global Freq", icon: PhoneIphone, component: GlobalCountFreq },
     { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
 ]/*
   [
